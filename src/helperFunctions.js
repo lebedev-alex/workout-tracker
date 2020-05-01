f22d852cad196e36bb8f84cf1958d95a55d4279c
A. Lebedev <lebedev96@gmail.com>
2020-05-01 15:38:25 +0300

    cleanup

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -2,10 +2,6 @@ export function countDaysInMonth(month, year) {
   return new Date(year, month, 0).getDate();
 }
 
-export function isEmpty(obj) {
-  return Object.keys(obj).length === 0;
-}
-
 export function addZ(n) {
   return n < 10 ? `0${n}` : `${n}`;
 }
